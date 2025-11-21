0cbd009e9053cc6fdab24882baeedc343f40419b
aina-c <aina.crespihromcova@gmail.com>
2025-11-21 17:45:06 +0100

    updeted yalm, not finished

--- src/dialogue_system.py
+++ src/dialogue_system.py
@@ -46,8 +46,41 @@ class FSMDialogueSystem:
 ##################### METHODS FOR MANAGING THE ROBOT'S EXPRESSIVENESS #####################
     # Method for controling the agents expressiveness. It should receive the parameters required for executing the different actions
     # and decide when to call each method
-    def expressiveness_system(self):
-        pass
+    def expressiveness_system(self, screen, speech=None, screen_content=None, animation=None):
+        """ BORRAR QUIZAS LUEGO
+        Coordinates and synchronizes all expressive channels of the robot.
+        
+        Args:
+            screen: The pygame screen object to draw on
+            speech: Audio file name to play (without .wav extension)
+            screen_content: String containing screen instructions in format "type;content&type;content*position"
+            animation: Animation folder name to play
+        """
+        
+        # Lists to track running processes
+        running_processes = []
+        
+        # 1. PLAY ANIMATION (if provided)
+        if animation:
+            # We'll run animation in a separate thread so it doesn't block speech
+            import threading
+            anim_thread = threading.Thread(target=self.play_animation, args=(screen, animation))
+            anim_thread.daemon = True
+            anim_thread.start()
+            running_processes.append(('animation', anim_thread))
+    
+        # 2. DISPLAY SCREEN CONTENT (if provided)
+        if screen_content:
+            self.display_multimedia_content(screen, screen_content)
+        
+        # 3. PLAY SPEECH and WAIT for it to finish (this is our main blocking call)
+        if speech:
+            self.robot_speech(speech)
+            # robot_speech should block until audio finishes playing
+        
+        # 4. If no speech but animation is running, wait for animation to complete
+        if not speech and animation:
+            anim_thread.join()
 
     # Method for playing the robot's voice
     def robot_speech(self):
@@ -71,6 +104,16 @@ class FSMDialogueSystem:
     def update_state(self):
         if self._current_state == 'introduction':
             self._current_state = 'explanation_needed'
+        elif self._current_state == 'explanation_needed':
+            user_answer = self.obtain_user_answer()
+            if user_answer == 'yes':
+                self._current_state = 'explanation'
+            else:
+                self._current_state = 'first_question'
+        elif self._current_state == 'explanation':
+            self._current_state = 'first_question'
+        elif 
+
         
     def execute_state(self, screen):
         if self._current_state == 'introduction':
